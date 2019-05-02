32eae5ff4c1fbe3b3e1b47c98475418e57f6e1e2
RaderF <36457068+RaderF@users.noreply.github.com>
2019-05-02 09:18:03 -0500

    Formatting changes
    tweaked still not working unit tests

--- app/src/test/java/com/example/spelltimer/CooldownModifierUnitTest.java
+++ app/src/test/java/com/example/spelltimer/CooldownModifierUnitTest.java
@@ -1,29 +1,34 @@
 package com.example.spelltimer;
 
+import android.widget.CheckBox;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class CooldownModifierUnitTest {
+//TODO remove null pointer exceptions from these tests
+public class CooldownModifierUnitTest extends MainActivity{
+    CheckBox hasBoots;
+    CheckBox hasInsight;
 
     @Test
     public void do_both_cdr_boxes_work() {
-        MainActivity.hasBoots.setChecked(true);
-        MainActivity.hasInsight.setChecked(true);
-        assertEquals(85, MainActivity.modifySpellCooldown(100));
+        hasBoots.setChecked(true);
+        hasInsight.setChecked(true);
+        assertEquals(85, modifySpellCooldown(100));
     }
 
     @Test
     public void does_only_boots_work() {
-        MainActivity.hasBoots.setChecked(true);
-        MainActivity.hasInsight.setChecked(false);
-        assertEquals(90, MainActivity.modifySpellCooldown(100));
+        hasBoots.setChecked(true);
+        hasInsight.setChecked(false);
+        assertEquals(90, modifySpellCooldown(100));
     }
 
     @Test
     public void does_only_insight_work() {
-        MainActivity.hasBoots.setChecked(false);
-        MainActivity.hasInsight.setChecked(true);
-        assertEquals(95, MainActivity.modifySpellCooldown(100));
+        hasBoots.setChecked(false);
+        hasInsight.setChecked(true);
+        assertEquals(95, modifySpellCooldown(100));
     }
 }
